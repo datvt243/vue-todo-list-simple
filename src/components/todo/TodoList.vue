ed30ea36da46f6ec9bed25543ee2ac0137555ede
Datto <votan.it@gmail.com>
2020-09-19 02:14:21 +0700

    fix

--- src/components/todo/TodoList.vue
+++ src/components/todo/TodoList.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="clearfix">
-    <ul class="todo-list mb-4">
-      <transition-group name="animate">
-        <li v-for="(item, index) in list" :key="index">
-          <div class="todo-item d-flex">
-            <div class="checked">
-              <div class="custom-control custom-switch">
-                <input type="checkbox" class="custom-control-input" 
-                  :id="'doto-' + index" 
-                  v-model="item.isComplete">
-                <label class="custom-control-label" 
-                  :for="'doto-' + index"></label>
-              </div>
-            </div>
-            <div class="content">
-              <p :class="{ del : item.isComplete }" v-if="!item.isEdit" @dblclick="emitEditTodo(item)">
-                {{ item.content }}
-              </p>
-              <div class="form-group" v-else>
-                <input type="text" class="form-control" 
-                  v-focus 
-                  v-model="item.content" 
-                  @blur="emitUpdateToDo(item)" 
-                  @keyup.enter="emitUpdateToDo(item)" 
-                  @keyup.esc="emitCanNotEdit(item)">
-              </div>
-            </div>
-            <div class="control">
-              <span class="btn" @click="emitDeleteToDo(index)">
-                <base-icon-delete />
-              </span>
+    <transition-group tag="ul" class="todo-list mb-4" name="animate">
+      <li v-for="(item, index) in list" :key="index">
+        <div class="todo-item d-flex">
+          <div class="checked">
+            <div class="custom-control custom-switch">
+              <input type="checkbox" class="custom-control-input" 
+                :id="'doto-' + index" 
+                v-model="item.isComplete">
+              <label class="custom-control-label" 
+                :for="'doto-' + index"></label>
             </div>
           </div>
-        </li>
-      </transition-group>
-    </ul>
+          <div class="content">
+            <p :class="{ del : item.isComplete }" v-if="!item.isEdit" @dblclick="emitEditTodo(item)">
+              {{ item.content }}
+            </p>
+            <div class="form-group" v-else>
+              <input type="text" class="form-control" 
+                v-focus 
+                v-model="item.content" 
+                @blur="emitUpdateToDo(item)" 
+                @keyup.enter="emitUpdateToDo(item)" 
+                @keyup.esc="emitCanNotEdit(item)">
+            </div>
+          </div>
+          <div class="control">
+            <span class="btn" @click="emitDeleteToDo(index)">
+              <base-icon-delete />
+            </span>
+          </div>
+        </div>
+      </li>
+    </transition-group>
   </div>
 </template>
 
@@ -42,13 +40,6 @@
 export default {
   name: 'TodoList',
   props: ['list'],
-  directives: {
-    focus: {
-      inserted: function (el) {
-        el.focus()
-      }
-    }
-  },
   methods: {
     emitEditTodo(item) {
       this.$emit('editTodo', item)
